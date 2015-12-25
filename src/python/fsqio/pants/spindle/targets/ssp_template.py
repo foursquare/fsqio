01f19f87efbe3c23afa54a07e33ece477122a90e
Patrick Lawson <pl@foursquare.com>
2015-12-24 22:23:34 -0500

    Revert "Opensource: Upgrade to Pants from 0.0.63 to 0.0.65."
    Upstream pants seems to have broken repl.
    
    This reverts commit 67b54c0e60460d537feb602e779df0254d435a3b.
    
    (sapling split of cdcc01f646e2a015a029f21b4ad7e7f19ca7edb7)

--- src/python/fsqio/pants/spindle/targets/ssp_template.py
+++ src/python/fsqio/pants/spindle/targets/ssp_template.py
@@ -11,7 +11,7 @@ from __future__ import (
   with_statement,
 )
 
-from pants.build_graph.resources import Resources
+from pants.backend.core.targets.resources import Resources
 
 
 class SspTemplate(Resources):
