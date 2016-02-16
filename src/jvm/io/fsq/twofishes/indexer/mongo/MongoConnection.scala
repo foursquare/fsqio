88de59c3d22729427c5972d93699489c622a6f87
Opensource <opensource@foursquare.com>
2016-02-16 12:30:34 -0500

    upgrade casbah library to v2.7.5 for Scala 2.11 cross-compiled version
    pom-resolve diff:
    --- .pants.d/pom-resolve/pom-resolve/4540f16f58a5d12f9a34838f54e3d1d7c0b619a1/classpath.txt	2016-02-16 11:13:24.000000000 -0500
    +++ .pants.d/pom-resolve/pom-resolve/fe714c59b322e1673943ad070197b6cba17d45ac/classpath.txt	2016-02-16 11:57:29.000000000 -0500
    @@ -1,4 +1,4 @@
    -FINGERPRINT: 4540f16f58a5d12f9a34838f54e3d1d7c0b619a1
    +FINGERPRINT: fe714c59b322e1673943ad070197b6cba17d45ac
     JSkills:jskills:0.1:jar:None
     alexa-skills-kit:alexa-skills-kit:1.1:jar:None
     ant:ant:1.6.5:jar:None
    @@ -64,7 +64,7 @@
     com.gilt:jerkson_2.10:0.6.7:jar:None
     com.github.fakemongo:fongo:1.6.2:jar:None
     com.github.fommil.netlib:core:1.1.2:jar:None
    -com.github.nscala-time:nscala-time_2.10:0.2.0:jar:None
    +com.github.nscala-time:nscala-time_2.10:1.0.0:jar:None
     com.github.rwl:jtransforms:2.4.0:jar:None
     com.github.scala-incubator.io:scala-io-core_2.10:0.4.3:jar:None
     com.github.scala-incubator.io:scala-io-file_2.10:0.4.3:jar:None
    @@ -583,9 +583,9 @@
     org.jsoup:jsoup:1.6.1:jar:None
     org.la4j:la4j:0.4.0:jar:None
     org.mockito:mockito-all:1.9.0:jar:None
    -org.mongodb:casbah-commons_2.10:2.6.4:jar:None
    -org.mongodb:casbah-core_2.10:2.6.4:jar:None
    -org.mongodb:casbah-query_2.10:2.6.4:jar:None
    +org.mongodb:casbah-commons_2.10:2.7.5:jar:None
    +org.mongodb:casbah-core_2.10:2.7.5:jar:None
    +org.mongodb:casbah-query_2.10:2.7.5:jar:None
     org.mongodb:mongo-java-driver:2.13.2_fs-c:jar:None
     org.mortbay.jetty:jetty:6.1.25-fs-c:jar:None
     org.mortbay.jetty:jetty-util:6.1.25-fs-c:jar:None
    
    (sapling split of 533bf8d82e4920bcd6d27b09fa4f190b504c7ae7)

--- src/jvm/io/fsq/twofishes/indexer/mongo/MongoConnection.scala
+++ src/jvm/io/fsq/twofishes/indexer/mongo/MongoConnection.scala
@@ -1,14 +1,14 @@
 // Copyright 2012 Foursquare Labs Inc. All Rights Reserved.
 package io.fsq.twofishes.indexer.mongo
 
-import com.mongodb.casbah.MongoConnection
+import com.mongodb.casbah.MongoClient
 
 object MongoIndexerConnection {
-  def apply() = {
+  def apply(): MongoClient = {
     Option(System.getProperty("mongodb.server")).map(_.split(":")).flatMap({
       case Array(host, port) =>
-        Some(MongoConnection(host, port.toInt))
+        Some(MongoClient(host, port.toInt))
       case _ => None
-    }).getOrElse(MongoConnection())
+    }).getOrElse(MongoClient())
   }
 }
