c062ca490932658659a0e898c35c0db23b4a04bc
Opensource <opensource@foursquare.com>
2016-02-22 20:14:25 -0500

    use separate package for the test of CompilerForNegativeTests
    (sapling split of c447f0f8da206e968d04c9e9504425cd2f8c8e7f)

--- test/jvm/io/fsq/util/compiler/test/test/TestOfCompilerForNegativeTests.scala
+++ test/jvm/io/fsq/util/compiler/test/test/TestOfCompilerForNegativeTests.scala
@@ -1,7 +1,8 @@
 // Copyright 2016 Foursquare Labs Inc. All Rights Reserved.
 
-package io.fsq.util.compiler.test
+package io.fsq.util.compiler.test.test
 
+import io.fsq.util.compiler.test.CompilerForNegativeTests
 import org.junit.{Assert => A, Test}
 
 case class TestCaseClass(v: Int)
@@ -9,7 +10,7 @@ case class TestCaseClass(v: Int)
 class TestOfCompilerForNegativeTests {
   @Test
   def testCompiler(): Unit = {
-    val compiler = new CompilerForNegativeTests(Seq("io.fsq.util.compiler.test.TestCaseClass"))
+    val compiler = new CompilerForNegativeTests(Seq("io.fsq.util.compiler.test.test.TestCaseClass"))
 
     val result1 = compiler.typeCheck("1 + 1")
     A.assertTrue(result1.isEmpty)
