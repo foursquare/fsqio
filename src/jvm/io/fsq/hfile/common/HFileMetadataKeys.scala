8a6ff0075aae5cc05347ed43e0e84859b1d31c7c
Jake Kaufman <jakeman@foursquare.com>
2016-05-16 12:21:04 -0400

    Move HFileMetadataKeys.scala to fsqio
    Splitting out deps for the HFileWriter.
    
    (sapling split of 5ecc0b620b9847f91bb16d309b92f9751d65201e)

---
+++ src/jvm/io/fsq/hfile/common/HFileMetadataKeys.scala
@@ -0,0 +1,21 @@
+// Copyright 2012 Foursquare Labs Inc. All Rights Reserved.
+
+package io.fsq.hfile.common
+
+
+trait HFileMetadataKeys {
+  val ThriftClassKey: String = "key.thrift.class"
+  val ThriftClassValue: String = "value.thrift.class"
+  val ThriftEncodingKey: String = "thrift.protocol.factory.class"
+  val TimestampKey: String = "generation.millis"
+  val TaskIdKey: String = "generation.taskId"
+
+  val LastKeyKey: String = "hfile.LASTKEY"
+  val AverageKeyLengthKey = "hfile.AVG_KEY_LEN"
+  val AverageValueLengthKey = "hfile.AVG_VALUE_LEN"
+  val ComparatorKey = "hfile.COMPARATOR"
+  val NumEntries = "hfile.NUM_ENTRIES"
+  val NumUniqueKeys = "hfile.NUM_UNIQUE_KEYS"
+}
+
+object HFileMetadataKeys extends HFileMetadataKeys
