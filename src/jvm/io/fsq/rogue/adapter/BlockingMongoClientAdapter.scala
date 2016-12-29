8de72a5d2b7e9c8ccae0f5908caf1624dbfb23bf
Jacob <jacob@foursquare.com>
2016-12-29 19:38:18 +0000

    add BlockingMongoClientAdapter
    Here we have the client adapter for the blocking api. This includes
    a wrapper class to carry around query results, which allows us to
    share most of the adapter logic with the async version. Implicits
    are also included to allow seamless conversion between wrapped and
    unwrapped values.
    
    Note the TODO around BlockingResult, ideally that class would be an
    extension of AnyVal so it does not incur a runtime cost. However, this
    gets us into erasure issues with the type abstracted wrapEmptyResult
    method. I don't think this is too much of an issue in practice, but
    certainly something we should work around if possible.
    
    (sapling split of 102aabd2508edcc7d2c88e4c0c4ece37f861c40c)

---
+++ src/jvm/io/fsq/rogue/adapter/BlockingMongoClientAdapter.scala
@@ -0,0 +1,40 @@
+// Copyright 2016 Foursquare Labs Inc. All Rights Reserved.
+
+package io.fsq.rogue.adapter
+
+import com.mongodb.client.MongoCollection
+
+
+object BlockingResult {
+  trait Implicits {
+    implicit def wrap[T](value: T): BlockingResult[T] = new BlockingResult[T](value)
+    implicit def unwrap[T](wrapped: BlockingResult[T]): T = wrapped.unwrap
+  }
+
+  object Implicits extends Implicits
+}
+
+// TODO(jacob): Currently this can't be an AnyVal due to erasure causing a clash in the
+//    wrapEmptyResult method below, see if there is a workaround.
+class BlockingResult[T](val value: T) {
+  def unwrap: T = value
+}
+
+
+object BlockingMongoClientAdapter {
+  type CollectionFactory[Document, MetaRecord, Record] = MongoCollectionFactory[
+    MongoCollection,
+    Document,
+    MetaRecord,
+    Record
+  ]
+}
+
+class BlockingMongoClientAdapter[Document, MetaRecord, Record](
+  collectionFactory: BlockingMongoClientAdapter.CollectionFactory[Document, MetaRecord, Record]
+) extends MongoClientAdapter[MongoCollection, Document, MetaRecord, Record, BlockingResult](
+  collectionFactory
+) with BlockingResult.Implicits {
+
+  override def wrapEmptyResult[T](value: T): BlockingResult[T] = new BlockingResult[T](value)
+}
