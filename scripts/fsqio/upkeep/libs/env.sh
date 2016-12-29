e582a0f82b8c033af700d00f8c3a723fecf2dff8
Mateo Rodriguez <mateo@foursquare.com>
2016-12-29 01:35:26 +0000

    Preserve any preset PANTS_CONFIG_OVERRIDE for fsqio
    The fsqio Travis build was surprisingly using nailguns -
    this was because the env file was clobbering the config
    that turns them off.
    
    (sapling split of aba36271b7d66e4ae31d1c2dbd788eabf8c2a3cf)

--- scripts/fsqio/upkeep/libs/env.sh
+++ scripts/fsqio/upkeep/libs/env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright 2015 Foursquare Labs Inc. All Rights Reserved.
 
-export PANTS_CONFIG_OVERRIDE="['pants.ini']"
+export PANTS_CONFIG_OVERRIDE=${PANTS_CONFIG_OVERRIDE:-"['pants.ini']"}
 
 # The script below runs the pants bootstrap task and exports PANTSBINARY. Basically a noop if the pants_version
 # hasn't changed. This could be hooked more properly into upkeep but we are waiting on the need to arise.
