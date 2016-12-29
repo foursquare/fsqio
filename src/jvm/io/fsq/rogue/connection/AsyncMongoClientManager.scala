cb8b11c28a31ff9fe780dafef7c05a6433fb3ef2
Jacob <jacob@foursquare.com>
2016-12-29 19:09:58 +0000

    MongoClientManager for the new CRUD api
    This is modeled after lift's MongoDB singleton. However, you will note
    that 1. it does not live in lift and 2. it is not a singleton. This
    removes the need to proxy all our mongo operations through lift and
    prevents the need for a lift fork to support the async client and new
    api.
    
    This also serves as an abstraction layer around the otherwise disjoint
    blocking and async mongo clients. However, even with most of the
    business logic pulled out you will notice that the two client managers
    are almost byte-for-byte identical. This is regretable, but unavoidable
    without resorting to structural types and reflection.
    
    (sapling split of f65db335e7bb5318f2b5c9c837b84469b840aa0d)

---
+++ src/jvm/io/fsq/rogue/connection/AsyncMongoClientManager.scala
@@ -0,0 +1,30 @@
+// Copyright 2016 Foursquare Labs Inc. All Rights Reserved.
+
+package io.fsq.rogue.connection
+
+import com.mongodb.ReadPreference
+import com.mongodb.async.client.{MongoClient, MongoCollection, MongoDatabase}
+
+
+/** MongoConnectionManager for the new async client. */
+class AsyncMongoClientManager
+extends MongoClientManager[MongoClient, MongoDatabase, MongoCollection] {
+
+  override protected def closeClient(client: MongoClient): Unit = {
+    client.close()
+  }
+
+  override protected def getDatabase(client: MongoClient, name: String): MongoDatabase = {
+    client.getDatabase(name)
+  }
+
+  override protected def getCollection[Document](
+    db: MongoDatabase,
+    name: String,
+    documentClass: Class[Document],
+    readPreferenceOpt: Option[ReadPreference]
+  ): MongoCollection[Document] = {
+    val collection = db.getCollection(name, documentClass)
+    readPreferenceOpt.map(collection.withReadPreference(_)).getOrElse(collection)
+  }
+}
---
+++ src/jvm/io/fsq/rogue/connection/MongoClientManager.scala
@@ -0,0 +1,88 @@
+// Copyright 2016 Foursquare Labs Inc. All Rights Reserved.
+
+package io.fsq.rogue.connection
+
+import com.mongodb.{MongoException, ReadPreference}
+import java.util.concurrent.ConcurrentHashMap
+import net.liftweb.util.ConnectionIdentifier
+import scala.collection.JavaConverters.mapAsScalaConcurrentMapConverter
+import scala.collection.concurrent.{Map => ConcurrentMap}
+
+
+/** Manages mongo connections and provides access to the client objects. This class is
+  * modeled after lift's MongoDB singleton, but in a way that abstracts out the type of
+  * client used (async vs blocking). Users must implement closeClient, getDatabase, and
+  * getCollection.
+  */
+abstract class MongoClientManager[MongoClient, MongoDatabase, MongoCollection[_]] {
+
+  private val dbs: ConcurrentMap[ConnectionIdentifier, (MongoClient, String)] = {
+    new ConcurrentHashMap[ConnectionIdentifier, (MongoClient, String)].asScala
+  }
+
+  /** Close a client connection, without removing it from the internal map. */
+  protected def closeClient(client: MongoClient): Unit
+
+  /** Get a MongoDatabase from a MongoClient. */
+  protected def getDatabase(client: MongoClient, name: String): MongoDatabase
+
+  /** Get a MongoCollection from a MongoDatabase.
+    * TODO(jacob): We should get rid of the option to send down a read preference here and
+    *     just use the one on the query.
+    */
+  protected def getCollection[Document](
+    db: MongoDatabase,
+    name: String,
+    documentClass: Class[Document],
+    readPreferenceOpt: Option[ReadPreference]
+  ): MongoCollection[Document]
+
+  def defineDb(
+    name: ConnectionIdentifier,
+    client: MongoClient,
+    dbName: String
+  ): Option[(MongoClient, String)] = {
+    dbs.put(name, (client, dbName))
+  }
+
+  def getDb(name: ConnectionIdentifier): Option[MongoDatabase] = {
+    dbs.get(name).map({
+      case (client, dbName) => getDatabase(client, dbName)
+    })
+  }
+
+  def getDbOrThrow(name: ConnectionIdentifier): MongoDatabase = {
+    getDb(name).getOrElse(throw new MongoException(s"Mongo not found: $name"))
+  }
+
+  /** Executes the given function with the specified database. Throws if the database does
+    * not exist. */
+  def use[T](name: ConnectionIdentifier)(f: MongoDatabase => T): T = f(getDbOrThrow(name))
+
+  /** Executes the given function with the specified database and collection. Throws if
+    * the database does not exist. */
+  def useCollection[
+    Document,
+    T
+  ](
+    name: ConnectionIdentifier,
+    collectionName: String,
+    documentClass: Class[Document],
+    readPreferenceOpt: Option[ReadPreference]
+  )(
+    f: MongoCollection[Document] => T
+  ): T = {
+    use(name)(db => {
+      val collection = getCollection(db, collectionName, documentClass, readPreferenceOpt)
+      f(collection)
+    })
+  }
+
+  /** Close all clients and clear the internal map. */
+  def closeAll(): Unit = {
+    dbs.valuesIterator.foreach({
+      case (client, _) => closeClient(client)
+    })
+    dbs.clear()
+  }
+}
