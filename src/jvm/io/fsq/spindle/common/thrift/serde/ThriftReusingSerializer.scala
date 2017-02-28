2e6c906f1c0089cc7c09e1001a1478fac4c45eca
Jackson Davis <jackson@foursquare.com>
2017-02-27 22:18:36 -0800

    Move ThriftReusingSerializer to fsq.io
    Summary: Now returns an (array, length) tuple to avoid the hadoop dep
    
    (sapling split of 94b8ce44c200854f76d70ddaa0e1fc812af491a5)

---
+++ src/jvm/io/fsq/spindle/common/thrift/serde/ThriftReusingSerializer.scala
@@ -0,0 +1,29 @@
+// Copyright 2017 Foursquare Labs Inc. All Rights Reserved.
+
+package io.fsq.spindle.common.thrift.serde
+
+import org.apache.thrift.protocol.TCompactProtocol
+import org.apache.thrift.transport.TIOStreamTransport
+import org.apache.thrift.{TBase, TByteArrayOutputStream, TFieldIdEnum}
+
+/**
+ * Custom serializer that reuses the internal byte array instead of defensively copying.
+ * NOTE: This means that you *must* use/copy the array before calling.
+ *
+ * As with the standard TSerializer, this is not threadsafe.
+ */
+class ThriftReusingSerializer[T <: TBase[_ <: TBase[_, _], _ <: TFieldIdEnum]] {
+  private[this] val protocolFactory = new TCompactProtocol.Factory()
+  private[this] val baos = new TByteArrayOutputStream()
+  private[this] val transport = new TIOStreamTransport(baos)
+  private[this] val prot = protocolFactory.getProtocol(transport)
+
+  /**
+   * Serialize the record out. Remember, the array is reused so you may not retain a reference between calls.
+   */
+  def serialize(t: T): (Array[Byte], Int) = {
+    baos.reset()
+    t.write(prot)
+    (baos.get, baos.len)
+  }
+}
