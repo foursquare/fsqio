de9aaa8fa98531b68283ea6ee4d6b955a5bef92c
Mateo Rodriguez <mateo@foursquare.com>
2017-05-22 18:02:48 +0000

    Decouple Go buildgen from fsqio Scala/Python buildgen
    I get 3s for ./pants binary src/go::
    
    To run Go buildgen, use `./pants bg`, it is not wired up to fs.
    
    (sapling split of ab0cef217aee72931e3cdb11d2d80a30dc90b1b3)

--- src/python/fsqio/pants/register.py
+++ src/python/fsqio/pants/register.py
@@ -3,6 +3,7 @@
 
 from __future__ import absolute_import
 
+from pants.contrib.go.tasks.go_buildgen import GoBuildgen
 from pants.goal.goal import Goal
 from pants.goal.task_registrar import TaskRegistrar as task
 from pants.task.task import Task
@@ -22,6 +23,10 @@ def register_goals():
     def execute(self):
       pass
 
+  Goal.register('bg', 'Buildgen scoped to only Go.')
+
   Goal.by_name('compile').uninstall_task('jvm-dep-check')
   Goal.by_name('dep-usage').uninstall_task('jvm')
   task(name='validate-graph', action=ForceValidation).install('gen')
+  task(name='go', action=GoBuildgen).install('bg')
+  Goal.by_name('buildgen').uninstall_task('go')
