00e9c090152d676dd52033bc0356894b423ff371
Jacob <jacob@foursquare.com>
2017-06-19 22:14:38 +0000

    add an IndexChecker to QueryUtilities
    De-singleton-ed, to override the logger used.
    
    (sapling split of 7d752c457e31eee133078f381e25c4a793d4d42b)

--- src/jvm/io/fsq/rogue/util/QueryUtilities.scala
+++ src/jvm/io/fsq/rogue/util/QueryUtilities.scala
@@ -2,16 +2,23 @@
 
 package io.fsq.rogue.util
 
+import io.fsq.rogue.IndexCheckerLogger
+import io.fsq.rogue.indexchecker.{IndexChecker, MongoIndexChecker}
+
 
 trait QueryUtilities[Result[_]] {
   def config: QueryConfig
+  def indexChecker: IndexChecker
   def logger: QueryLogger[Result]
   def transformer: QueryTransformer
   def validator: QueryValidator
 }
 
-class DefaultQueryUtilities[Result[_]] extends QueryUtilities[Result] {
+class DefaultQueryUtilities[Result[_]] extends QueryUtilities[Result] { self =>
   override val config: QueryConfig = new DefaultQueryConfig
+  override val indexChecker: IndexChecker = new MongoIndexChecker {
+    override def logger: IndexCheckerLogger = self.logger
+  }
   override val logger: QueryLogger[Result] = new DefaultQueryLogger[Result]
   override val transformer: QueryTransformer = new DefaultQueryTransformer
   override val validator: QueryValidator = new DefaultQueryValidator
