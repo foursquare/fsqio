6c37a2773b07be2887a9ad147b9025df38ea4c9b
Tansy Arron <tansy@foursquare.com>
2017-07-17 17:59:58 +0000

    Fix logging interpolation (#435)
    (sapling split of 5ea263280ebaf79421bdc85884ba61ac40cb2b69)

--- src/jvm/io/fsq/hfile/reader/concrete/mmap/ConcreteHFileScanner.scala
+++ src/jvm/io/fsq/hfile/reader/concrete/mmap/ConcreteHFileScanner.scala
@@ -112,7 +112,7 @@ class ConcreteHFileScanner(reader: ConcreteHFileReader) extends HFileScanner wit
 
     // special case when the inputKey is smaller than the first key
     if (compareWithFirstKey < 0 ) {
-      logger.debug("seekToInner: found a key lower than the current key in block %", startIndex)
+      logger.debug("seekToInner: found a key lower than the current key in block %d", startIndex)
       setCurBuffer()
       -1
     } else {
