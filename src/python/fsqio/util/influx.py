12e277cfe00e8092bd7db2d6d362fe5d6faf2907
Tansy Arron-Walker <tansy@foursquare.com>
2017-09-22 15:27:17 -0400

    Improve jenkins stats collection (#1983)
    - Refactor our stats collection code to run as a batch job instead
    of at the end of each jenkins job.
    - Add more stats to the daily aggregation task
    - Provide a formatting helper method to deal with influx's crazy formatting rules.
    - Fix some gnarly bugs in timezone handling with liberal applications of UTC.
    
    
    
    
    (sapling split of b744405d779e8cf36fcb20f9cf69380cc1aea01a)

--- src/python/fsqio/util/influx.py
+++ src/python/fsqio/util/influx.py
@@ -5,60 +5,91 @@ from __future__ import absolute_import
 
 from datetime import datetime
 import logging
+import math
 
+import pytz
 import requests
+import six
 
 
 logger = logging.getLogger(__name__)
 
+UTC_DATETIME_MIN = datetime.min.time().replace(tzinfo=pytz.UTC)
+
 
 def _backslash_escape(value, to_escape):
   for char in to_escape:
-    value = value.replace(char, '\\{}'.format(char))
+    value = value.replace(char, b'\\{}'.format(char))
   return value
 
 
-def datetime_to_nanos_since_epoch(dt):
-  seconds_since_epoch = int((dt - datetime(1970, 1, 1)).total_seconds())
+def datetime_to_nanos_since_utc_epoch(dt):
+  seconds_since_epoch = int((dt - datetime(1970, 1, 1, tzinfo=pytz.UTC)).total_seconds())
   return seconds_since_epoch * 10 ** 9
 
 
-def truncate_date_to_midnight(date):
-  return datetime.combine(date, datetime.min.time())
+def truncate_date_to_utc_midnight(date):
+  return datetime.combine(date, UTC_DATETIME_MIN)
+
 
 def format_tags_or_fields(data):
-  result = ','.join(
-        '{key}={value}'.format(
-          key=_backslash_escape(key, [',', ' ', '=']),
-          value=_backslash_escape(value, [',', ' ', '=']),
-        )
-        for key, value in sorted(data.items())
-      )
+  result = b','.join(
+    b'{key}={value}'.format(
+      key=_backslash_escape(key, [',', ' ', '=']),
+      value=_backslash_escape(value, [',', ' ', '=']),
+    )
+    for key, value in sorted(data.items())
+  )
   return result
+
+
+def influx_format(value):
+  """Format fields in the way influx expects. Returns bytes
+
+  String types must be double quoted or influx will interpret them as bools.
+  Numbers not followed by 'i' are interpreted as floats,
+  """
+  if isinstance(value, six.integer_types):
+    return b'{}i'.format(value)
+  elif isinstance(value, six.text_type):
+    return b'"{}"'.format(value.encode('utf-8'))
+  elif isinstance(value, six.binary_type):
+    # Check bytes are utf-8. Raise an error if they're not.
+    value.decode('utf-8')
+    return b'"{}"'.format(value)
+  elif value is None:
+    return b'0i'
+  elif isinstance(value, float):
+    if math.isnan(value) or math.isinf(value):
+      raise ValueError
+    return b'{}'.format(value)
+  else:
+    raise TypeError
+
 
 def build_influx_line(measurement, tags, fields, dt):
   """ An influx line consists of [key] [fields] [timestamp] separated by spaces.
   The key is the measurement name and any optional tags separated by commas.
   Fields are key-value metrics associated with the measurement.
-  note, numbers not followed by 'i' will be stored as floats.
   The Timestamp value is an integer representing nanoseconds since the epoch
   eg:
   measurement[,tag_key1=tag_value1...] field_key=field_value[,field_key2=field_value2] [timestamp]
   """
-  line_template = '{measurement},{tags} {fields} {timestamp}'
+  line_template = b'{measurement},{tags} {fields} {timestamp}'
   return line_template.format(
     measurement=_backslash_escape(measurement, [',', ' ']),
     tags=format_tags_or_fields(tags),
     fields=format_tags_or_fields(fields),
-    timestamp=datetime_to_nanos_since_epoch(dt),
+    timestamp=datetime_to_nanos_since_utc_epoch(dt),
   )
 
 
-def write_lines_to_influx(influxdb_url, db, lines):
+def write_lines_to_influx(influxdb_url, db, lines, session=None):
+  session = session or requests.Session()
   logger.info("Send points to Influx")
   influx_data = '\n'.join(lines)
-  response = requests.post(
-    '{}/write'.format(influxdb_url),
+  response = session.post(
+    url='{}/write'.format(influxdb_url),
     params={'db': db},
     data=influx_data,
   )
---
+++ test/python/fsqio_test/util/test_influx.py
@@ -0,0 +1,104 @@
+# coding=utf-8
+# Copyright 2017 Foursquare Labs Inc. All Rights Reserved.
+
+from __future__ import absolute_import, unicode_literals
+
+from datetime import datetime
+
+from mock import MagicMock
+import pytz
+import requests
+import unittest2 as unittest
+
+from fsqio.util.influx import (
+  build_influx_line,
+  datetime_to_nanos_since_utc_epoch,
+  influx_format,
+  truncate_date_to_utc_midnight,
+  write_lines_to_influx,
+)
+
+
+class MockResponse(object):
+  def __init__(self, status_code=200, content='content'):
+    self.status_code = status_code
+    self.content = content
+
+  def raise_for_status(self):
+    raise Exception
+
+
+class TestInflux(unittest.TestCase):
+
+  def setUp(self):
+    self.timestamp = 1494588400
+    self.timestamp_in_nanos = self.timestamp * 10 ** 9
+    self.utc_dt = datetime(2017, 5, 12, 11, 26, 40, tzinfo=pytz.UTC)
+
+  def test_datetime_to_nanos_since_utc_epoch(self):
+    self.assertEqual(self.timestamp_in_nanos, datetime_to_nanos_since_utc_epoch(self.utc_dt))
+    naive_dt = datetime.fromtimestamp(self.timestamp)
+    with self.assertRaises(TypeError):
+      datetime_to_nanos_since_utc_epoch(naive_dt)
+
+  def test_truncate_date_to_utc_midnight(self):
+    self.assertEqual(datetime(2017, 5, 12, 0, 0, tzinfo=pytz.UTC), truncate_date_to_utc_midnight(self.utc_dt))
+
+  def test_influx_format(self):
+    self.assertEqual(b'12i', influx_format(12))
+    self.assertEqual('0.1', influx_format(0.1))
+    with self.assertRaises(ValueError):
+      influx_format(float('NaN'))
+    with self.assertRaises(ValueError):
+      influx_format(float('inf'))
+    self.assertEqual(b'"aString"', influx_format('aString'))
+    self.assertEqual(b'"aString"', influx_format(u'aString'))
+    with self.assertRaises(UnicodeDecodeError):
+      influx_format(b'\x80abc')
+    with self.assertRaises(UnicodeDecodeError):
+      influx_format(u'å'.encode('latin-1'))
+    self.assertEqual(b'0i', influx_format(None))
+
+  def test_build_influx_line(self):
+    influx_line = build_influx_line(
+      measurement='duration',
+      tags={'some_tag': 'a_tag_value'},
+      fields={
+        'string': '"bar"',
+        'int': '42i',
+        'float': '1.2',
+        'empty': '0i',
+      },
+      dt=self.utc_dt,
+    )
+    self.assertEqual(
+      influx_line,
+      'duration,some_tag=a_tag_value empty=0i,float=1.2,int=42i,string="bar" 1494588400000000000'
+    )
+
+  def test_write_lines_to_influx(self):
+    good_session = requests.Session()
+    good_session.post = MagicMock(return_value=MockResponse())
+    write_lines_to_influx(
+      session=good_session,
+      lines=['not a real line', 'fakefakefakefake'],
+      influxdb_url='influxdb_url',
+      db='fakedb',
+    )
+    good_session.post.assert_called_once_with(
+      url='influxdb_url/write',
+      params={'db': u'fakedb'},
+      data=u'not a real line\nfakefakefakefake',
+    )
+    bad_session = requests.session()
+    bad_session.post = MagicMock(return_value=MockResponse(status_code=404))
+    with self.assertRaises(Exception):
+      write_lines_to_influx(
+        session=bad_session,
+        lines=['not a real line', 'fakefakefakefake'],
+        influxdb_url='influxdb_url',
+        db='fakedb',
+      )
+
+  def tearDown(self):
+    pass
