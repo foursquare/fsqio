c951880c03deb16db7af69bd2aaa44b842bea579
Opensource <opensource@foursquare.com>
2017-09-27 09:48:19 -0400

    Add TwoFishes geocoder to preEnrich (#2015)
    * Add TwoFishes geocoder to preEnrich
    
    * Code review
    
    * Code review 2
    
    * Fixlint
    
    * Buildgen
    
    (sapling split of d670f9b15b7196ff00d24c2070c13cfcf23c4c0e)

--- src/jvm/io/fsq/twofishes/core/YahooWoeTypes.scala
+++ src/jvm/io/fsq/twofishes/core/YahooWoeTypes.scala
@@ -9,6 +9,13 @@ object YahooWoeTypes {
   val order = List(POSTAL_CODE, AIRPORT, POI, SUBURB, TOWN, ADMIN3, ADMIN2, ADMIN1, COUNTRY)
   val orderMap = order.zipWithIndex.toList.toMap
 
+  val postalCodeWoeTypes: Set[YahooWoeType] = Set(YahooWoeType.POSTAL_CODE)
+  val neighborhoodWoeTypes: Set[YahooWoeType] = Set(YahooWoeType.SUBURB, YahooWoeType.TOWN)
+  val cityWoeTypes: Set[YahooWoeType] = Set(YahooWoeType.SUBURB, YahooWoeType.TOWN, YahooWoeType.ADMIN3)
+  val countyWoeTypes: Set[YahooWoeType] = Set(YahooWoeType.ADMIN2)
+  val provinceWoeTypes: Set[YahooWoeType] = Set(YahooWoeType.ADMIN2, YahooWoeType.ADMIN1)
+  val countryWoeTypes: Set[YahooWoeType] = Set(YahooWoeType.COUNTRY)
+
   def getOrdering(woetype: YahooWoeType): Int = {
     orderMap.get(woetype).getOrElse(-1)
   }
