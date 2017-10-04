fe18b13b1a0cfcbe8a415c961ee632decbfa78d0
Bill Cauchois <wcauchois@foursquare.com>
2017-10-04 15:22:07 -0400

    add AssertException helper for unit testing (#2243)
    * add AssertException helper for unit testing
    
    - while in the process of updating the wiki page on testing, i found
      that the recommended way to test for exceptions was to write boilerplate
      that could easily be abstracted. specsmatchers has the must throwA[Foo] syntax,
      but we don't have a pure-junit helper. this is that helper.
    - while in the process of writing AssertException, i found that this had already
      been implemented by benjy as AssertExceptions. however, his version only supports
      matching on class and a regex of the message. my idea was to use a PartialFunction
      on exceptions, which i think should lead to a nicer interface.
    - keep benjy's idea alive as a matchesClassAndRegex helper, but the main interface
      via apply() will use the PartialFunction.
    - will update the testing wiki to recommend this method.
    
    * missing space in comment (scala style)
    
    * change && to ||, thanks jacob!
    
    (sapling split of c2adf02ca3f35b56ca36ffd7821595889fd33bf8)

---
+++ test/jvm/io/fsq/common/testing/AssertException.scala
@@ -0,0 +1,85 @@
+// Copyright 2017 Foursquare Labs Inc. All Rights Reserved.
+
+package io.fsq.common.testing
+
+import org.junit.{Assert => A}
+import io.fsq.common.scala.Identity._
+
+/**
+ * This helper for JUnit tests allows you to verify that a block of code throws
+ * an exception. Pass in a PartialFunction from Exceptions to Booleans that should
+ * match and return true if the exception was expected. If this function does not match,
+ * or the code does not throw an exception, an assertion will be raised.
+ *
+ * Example:
+ *   AssertException("Should throw", { case e: Exception => e.getMessage.contains("thing") })({
+ *     throw new Exception("thing!") 
+ *   })
+ */
+object AssertException {
+  /**
+   * @param msg Message to be shown on failure.
+   * @param exceptionMatcher Should return true if the exception was expected.
+   * @param body The thunk to execute that should throw an exception.
+   */
+  def apply(
+    msg: String,
+    exceptionMatcher: PartialFunction[Exception, Boolean]
+  )(body: => Unit): Unit = {
+    def doFail() = if (msg !=? "") {
+      A.fail(msg)
+    } else {
+      A.fail()
+    }
+
+    try {
+      body
+      doFail()
+    } catch {
+      case e: Exception => {
+        if (!exceptionMatcher.isDefinedAt(e) || !exceptionMatcher(e)) {
+          doFail()
+        }
+      }
+    }
+  }
+
+  /**
+   * @param exceptionMatcher Should return true if the exception was expected.
+   * @param body The thunk to execute that should throw an exception.
+   */
+  def apply(
+    exceptionMatcher: PartialFunction[Exception, Boolean]
+  )(body: => Unit): Unit = {
+    apply(
+      "",
+      exceptionMatcher
+    )(body)
+  }
+
+  /**
+   * You might find this convenient if you want to verify that an exception matches
+   * a given class and that the message matches a regex. Emits descriptive error
+   * messages.
+   *
+   * @param klass Exception must match this class (e.g. classOf[Foo]).
+   * @param msgRegex Regular expression to match against the exception message.
+   * @param body The thunk to execute that should throw an exception.
+   */
+  def matchesClassAndRegex[E <: Exception](
+    klass: Class[E],
+    msgRegex: String
+  )(body: => Unit): Unit = {
+    try {
+      body
+      A.fail(s"Should have thrown a ${klass.getSimpleName}, but succeeded")
+    } catch {
+      case e: Exception if e.getClass == klass => {
+        if (!e.getMessage.matches(msgRegex)) {
+          A.fail(s"""Message failed to match regex "${msgRegex}": "${e.getMessage}"""")
+        }
+      }
+      case e: Exception => throw e
+    }
+  }
+}
