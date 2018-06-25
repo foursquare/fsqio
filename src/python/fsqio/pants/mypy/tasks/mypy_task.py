8ebc981bc7f26643b9c0b5687802211b94b39471
Eric Arellano <earellano@foursquare.com>
2018-06-25 10:52:13 -0400

    Implement `./fs mypy` to run locally (#7112)
    Hooks up the ./fs mypy command that we had initially implemented so that it is able to run with sensible defaults.
    
    Main default settings:
    - Hardcodes running in Python2 mode
    - No longer raising TaskError if there is a mypy issue. Our code base has too many legacy issues and MyPy and type hints are imperfect. Will print the errors without stopping pants.
    - Ignores imports without type stubs. This is recommended to reduce noise, and the unfortunate reality that not every library has typing support yet. This should be improved over the years as more people use typing.
    - Silently follow errors from imports. This is recommended for new code bases to make errors less overwhelming and focus on the file at hand.
    
    --
    
    Note - uses local install
    This uses the user's local python3 and mypy, rather than from Pants. This is a temporary solution to enable mypy functionality now - as a separate task I will integrate this into pants so that users don't have to download anything.
    
    I added a check for if mypy and python3 are on the local PATH. If they aren't, then we raise a human readable error explaining how to install.
    
    (sapling split of 21bff41363c97c94ea0a17b62f760532771e6876)

--- src/python/fsqio/pants/mypy/tasks/mypy_task.py
+++ src/python/fsqio/pants/mypy/tasks/mypy_task.py
@@ -7,23 +7,25 @@ from __future__ import (
   generators,
   nested_scopes,
   print_function,
-  unicode_literals,
   with_statement,
 )
 
 import os
 import subprocess
+from textwrap import dedent
 
 from pants.backend.python.targets.python_binary import PythonBinary
-from pants.backend.python.targets.python_library import PythonLibrary
+from pants.backend.python.targets.python_library import PythonLibrary, PythonTarget
 from pants.backend.python.targets.python_tests import PythonTests
+from pants.base.build_environment import get_buildroot
 from pants.base.exceptions import TaskError
 from pants.base.workunit import WorkUnit, WorkUnitLabel
 from pants.task.task import Task
+from typing import List, Set
 
 
 class MypyTask(Task):
-  """Invoke the mypy static type analzyer for Python."""
+  """Invoke the mypy static type analyzer for Python."""
 
   _PYTHON_SOURCE_EXTENSION = '.py'
 
@@ -32,6 +34,12 @@ class MypyTask(Task):
     super(MypyTask, cls).register_options(register)
     register('--py3-path', type=str, default='python3',
              help='Path to Python 3 interpreter')
+    register('--warning-only', type=bool, default=True,
+             help='Determines if Pants stops due to error.')
+    register('--ignore-missing-imports', type=bool, default=True,
+             help='Ignore missing type stubs if not found. Useful for mostly untyped code bases.')
+    register('--config-file', type=str, default='build-support/fsqio/mypy/mypy.ini',
+             help='Path to .ini file, relative to build root.')
 
   @classmethod
   def supports_passthru_args(cls):
@@ -42,12 +50,14 @@ class MypyTask(Task):
 
   @staticmethod
   def is_non_synthetic_python_target(target):
+    # type: (PythonTarget) -> bool
     return not target.is_synthetic and isinstance(target, (PythonLibrary, PythonBinary, PythonTests))
 
   def _calculate_python_sources(self, targets):
+    # type: (List[PythonTarget]) -> List[str]
     """Generate a set of source files from the given targets."""
     python_eval_targets = filter(self.is_non_synthetic_python_target, targets)
-    sources = set()
+    sources = set()  # type: Set[str]
     for target in python_eval_targets:
       sources.update(
         source for source in target.sources_relative_to_buildroot()
@@ -55,13 +65,48 @@ class MypyTask(Task):
       )
     return list(sources)
 
+  # TODO(earellano): users shouldn't have to install Python3 and MyPy locally. This is a temporary solution
+  def _assert_mypy_and_python3_installed_locally(self):
+    # type: () -> None
+    def program_exists_on_path(program_name):  # see https://github.com/pydanny/whichcraft/blob/master/whichcraft.py
+      # type: (str) -> bool
+      path = os.environ.get("PATH", os.defpath)
+      path_list = path.split(os.pathsep)
+      return any(os.path.exists(os.path.join(path_dir, program_name))
+                 for path_dir in path_list)
+
+    def raise_not_installed_error(program_name, install_command):
+      # type: (str, str) -> None
+      raise TaskError(dedent('''
+        {0} not found on local path. Install with `{1}`.\n
+        (Note this is a temporary solution - Pants will soon automatically install this for you.)
+        '''.format(program_name, install_command)))
+
+    py3_interpreter = self.get_options().py3_path
+    if not program_exists_on_path(py3_interpreter):
+      raise_not_installed_error('python3', 'brew install python3')
+    if not program_exists_on_path('mypy'):
+      raise_not_installed_error('mypy', 'pip3 install mypy')
+
   def execute(self):
+    # type: () -> None
+    self._assert_mypy_and_python3_installed_locally()
+
+    py3_interpreter = self.get_options().py3_path
+
     sources = self._calculate_python_sources(self.context.target_roots)
     if not sources:
       self.context.log.debug('No Python sources to check.')
       return
 
-    cmd = [self.get_options().py3_path, '-m', 'mypy', '-2'] + self.get_passthru_args() + sources
+    mypy_options = [
+      '--py2',  # run in Python2 mode
+      '--config-file={}'.format(os.path.join(get_buildroot(), self.get_options().config_file))
+    ]
+    if self.get_options().ignore_missing_imports:
+      mypy_options.append('--ignore-missing-imports')
+
+    cmd = [py3_interpreter, '-m', 'mypy'] + mypy_options + self.get_passthru_args() + sources
     self.context.log.debug('mypy command: {}'.format(' '.join(cmd)))
 
     with self.context.new_workunit(
@@ -70,6 +115,6 @@ class MypyTask(Task):
       log_config=WorkUnit.LogConfig(level=self.get_options().level, colors=self.get_options().colors),
       cmd=' '.join(cmd)) as workunit:
       proc = subprocess.Popen(cmd, stdout=workunit.output('stdout'), stderr=subprocess.STDOUT)
-      returncode = proc.wait()
-      if returncode != 0:
-        raise TaskError('mypy failed: code={}'.format(returncode))
+      return_code = proc.wait()
+      if not self.get_options().warning_only and return_code != 0:
+        raise TaskError('mypy failed: code={}'.format(return_code))
