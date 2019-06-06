488ad93d4f5701e29a01a81d10194ed76961f128
Mateo Rodriguez <mateo@foursquare.com>
2019-06-06 16:20:04 -0400

    Add Fsq.io publish jar docs (#15112)
    * Add Fsq.io publish jar docs
    
    This fixes the Fsqio publish and moves the instructions from the wiki
    to the codebase. They were written several years ago and need pretty major updating
    I am going to ask we land them as-is and update while walking through them
    
    * update readme
    
    (sapling split of 2648ec2d9db2b6082d75de8b15943a6bceaf67e1)

--- src/python/fsqio/pants/publish/register.py
+++ src/python/fsqio/pants/publish/register.py
@@ -1,6 +1,13 @@
 # coding=utf-8
 # Copyright 2016 Foursquare Labs Inc. All Rights Reserved.
 
+"""
+# TODO(mateo): Upstream Pants javadoc chokes on scala_libraries with mixed *.java and
+# *.scala sources, because the javadoc task only checks for target.has_sources('*java').
+# We hack the fix by also opting out if isinstance(target, ScalaLibrary) but a bug should be
+# filed upstream.
+"""
+
 from __future__ import absolute_import, division, print_function
 
 import os
@@ -19,7 +26,7 @@ from pants.build_graph.build_file_aliases import BuildFileAliases
 oss_sonatype_repo = Repository(
   name='oss_sonatype_repo',
   url='https://oss.sonatype.org/#stagingRepositories',
-  push_db_basedir=os.path.join(get_buildroot(), 'build-support', 'fsqio', 'ivy', 'pushdb'),
+  push_db_basedir=os.path.join(get_buildroot(), 'build-support', 'fsqio', 'pushdb'),
 )
 
 
@@ -53,8 +60,3 @@ def build_file_aliases():
       'oss_sonatype_repo': oss_sonatype_repo,
     },
   )
-
-# TODO: Upstream Pants javadoc chokes on scala_libraries with mixed *.java and
-# *.scala sources, because the javadoc task only checks for target.has_sources('*java').
-# We hack the fix by also opting out if isinstance(target, ScalaLibrary) but a bug should be
-# filed upstream.
